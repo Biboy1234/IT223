24af7e5d1f38a1bed76d9988c4e3e6f822552901
Jayde Boy <126141749+Biboy1234@users.noreply.github.com>
2023-03-13 16:38:57 +0800

    Add files via upload

---
+++ Biboygwapo.java
@@ -0,0 +1,17 @@
+
+package biboygwapo;
+
+/**
+ *
+ * @author Jayde
+ */
+public class Biboygwapo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
